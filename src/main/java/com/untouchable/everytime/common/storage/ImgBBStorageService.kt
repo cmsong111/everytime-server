34289edbfdf73c1b584cd130cfb2c2337dacf37d
Namju Kim <cmsong111@naver.com>
2025-01-30 17:47:19 +0900

    refactor: restTemplate -> restClient

--- src/main/java/com/untouchable/everytime/common/storage/ImgBBStorageService.kt
+++ src/main/java/com/untouchable/everytime/common/storage/ImgBBStorageService.kt
@@ -3,13 +3,11 @@ package com.untouchable.everytime.common.storage
 import io.github.oshai.kotlinlogging.KotlinLogging
 import java.util.Base64
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.http.HttpEntity
-import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpMethod
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.util.LinkedMultiValueMap
-import org.springframework.web.client.RestTemplate
+import org.springframework.util.MultiValueMap
+import org.springframework.web.client.RestClient
 import org.springframework.web.multipart.MultipartFile
 
 @Component
@@ -17,28 +15,23 @@ class ImgBBStorageService(
     @Value("\${storage.imgbb.api-key}") private val apiKey: String,
 ) : StorageService {
 
-    private val restTemplate: RestTemplate = RestTemplate()
+    private val restClient: RestClient = RestClient.create()
 
     private fun uploadImage(image: MultipartFile): String {
         // Multipart/form-data 형식으로 이미지를 Base64로 인코딩하여 전송
-        val requestEntity = HttpEntity(
-            LinkedMultiValueMap<String, Any>().apply {
-                add("image", Base64.getEncoder().encodeToString(image.bytes))
-            }, HttpHeaders().apply {
-                contentType = MediaType.MULTIPART_FORM_DATA
-            }
-        )
+        val parts: MultiValueMap<String, Any> = LinkedMultiValueMap()
+        parts.add("image", Base64.getEncoder().encodeToString(image.bytes))
 
-        // API 호출 및 응답 처리
-        val response = restTemplate.exchange(
-            "$IMGBB_API_URL?key=$apiKey",
-            HttpMethod.POST,
-            requestEntity,
-            Map::class.java
-        )
+
+        val response: Map<*, *>? = restClient.post()
+            .uri("$IMGBB_API_URL?key=$apiKey")
+            .contentType(MediaType.MULTIPART_FORM_DATA)
+            .body(parts)
+            .retrieve()
+            .body(Map::class.java)
 
         // 업로드 결과에서 이미지 URL 추출
-        val data = response.body?.get("data") as Map<*, *>?
+        val data = response?.get("data") as Map<*, *>?
         return data?.get("url").toString()
     }
 
